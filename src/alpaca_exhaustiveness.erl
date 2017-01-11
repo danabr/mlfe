e046afddf6e33ca3da2467b628a1ea177edf4ef5
Daniel Abrahamsson <hamsson@gmail.com>
2017-01-11 21:53:28 +0100

    Cleanup.

--- src/alpaca_exhaustiveness.erl
+++ src/alpaca_exhaustiveness.erl
@@ -1,3 +1,15 @@
+%% Note We do not deal with guards, since they can be arbitrary expressions.
+%% Here are two examples that both cover the full set of integers:
+%%   match i with
+%%     x, x < 50 -> :ok
+%%     x, x = 50 -> :ok
+%%     x, x > 50 -> :ok
+%%
+%%   match i with
+%%     x, x % 2 = 0 -> :ok
+%%     x, x % 2 = 1 -> :ok
+%%
+%% Note: We don't handle union types!
 -module(alpaca_exhaustiveness).
 
 -export([check_exhaustiveness/1]).
@@ -54,18 +66,14 @@ test_exhaustiveness(#alpaca_fun_def{versions=FunArgPatterns}=F, FunArgTypes) ->
     []              -> [];
     MissingPatterns -> [{partial_function, F, MissingPatterns}]
   end.
-%% For each argument a with index i:
-%%   For each constructor of typeof(a):
-%%     * Extract matching patterns, assuming all previous argument patterns match.
-%%     * For each constructor argument c:
-%%       Assert that there is at least one pattern covering c.
+
 exhaustiveness(FunArgTypes, FunArgPatterns) ->
   NumArgs = length(FunArgTypes),
   flatmap_with_index(FunArgTypes, fun(FunArgType, ArgIx) ->
     Patterns = extract_patterns(FunArgPatterns, ArgIx),
     ArgsAfter = NumArgs-ArgIx-1,
     map(constructors(FunArgType), fun(Constr) ->
-      case covered(Constr, Patterns) of
+      case lists:any(fun(P) -> covered(Constr, P) end, Patterns) of
         true -> [];
         false ->
           MissingPattern = lists:duplicate(ArgIx, '_') ++
@@ -84,7 +92,7 @@ map_with_index([X|Rest], Ix, Acc, F) ->
 
 map(List, F) -> lists:map(F, List).
 
-constructors(#adt{members=Constructors}=A) ->
+constructors(#adt{members=Constructors}) ->
   lists:flatmap(fun constructors/1, Constructors);
 constructors({t_adt_cons, N}=C) when is_list(N) -> [C];
 constructors({t_arrow, _, _})     -> ['_'];
@@ -137,23 +145,24 @@ extract_patterns(FunArgPatterns, ArgIx) ->
 extract_pattern(#alpaca_fun_version{args=Args}, Ix) ->
   lists:nth(Ix+1, Args).
 
-covered('_', Patterns) ->
-  lists:any(fun matches_wildcard/1, Patterns);
-covered({t_adt_cons, CNeedle}, Patterns) ->
-  lists:any(fun(P) -> matches_constructor(P, CNeedle) end, Patterns);
-covered({t_bool, Boolean}, Patterns) ->
-  lists:any(fun(P) -> matches_bool(P, Boolean) end, Patterns);
-covered({t_list, empty}, Patterns) ->
-    lists:any(fun matches_empty_list/1, Patterns);
-covered({t_list, Elem}, Patterns) ->
-  lists:any(fun(P) -> matches_list(P, Elem) end, Patterns);
-covered(t_map, _Patterns) -> true;
-covered({t_record, Assignments}, Patterns) ->
-  lists:any(fun(P) -> matches_record(P, Assignments) end, Patterns);
-covered({t_tuple, TSize, Ix, MC}, Patterns) ->
-  lists:any(fun(P) -> matches_tuple(P, TSize, Ix, MC) end, Patterns);
-covered(t_unit, Patterns) ->
-  lists:any(fun matches_unit/1, Patterns).
+covered('_', Pattern)                      ->
+  matches_wildcard(Pattern);
+covered({t_adt_cons, CNeedle}, Pattern)    ->
+  matches_constructor(Pattern, CNeedle);
+covered({t_bool, Boolean}, Pattern)        ->
+  matches_bool(Pattern, Boolean);
+covered({t_list, empty}, Pattern)          ->
+    matches_empty_list(Pattern);
+covered({t_list, Elem}, Pattern)           ->
+  matches_list(Pattern, Elem);
+covered(t_map, _Pattern)                   ->
+  true;
+covered({t_record, Assignments}, Pattern)  ->
+  matches_record(Pattern, Assignments);
+covered({t_tuple, TSize, Ix, MC}, Pattern) ->
+  matches_tuple(Pattern, TSize, Ix, MC);
+covered(t_unit, Pattern)                   ->
+  matches_unit(Pattern).
 
 to_val(#alpaca_type_apply{name={type_constructor, _, CName}}) -> {t_adt_cons, CName};
 to_val({boolean, _Line, Val}) -> {t_bool, Val};
@@ -171,19 +180,19 @@ matches_empty_list({nil, _}) -> true;
 matches_empty_list(C)        -> matches_wildcard(C).
 
 matches_list(#alpaca_cons{head=H, tail=T}, E) ->
-  covered(E, [H]) andalso matches_wildcard(T);
+  covered(E, H) andalso matches_wildcard(T);
 matches_list(P, _E) ->
   matches_wildcard(P).
 
 matches_record(#alpaca_record{members=Ms}, Assignments) ->
   lists:all(fun(#alpaca_record_member{name=N, val=P}) ->
-    covered(maps:get(N, Assignments), [P])
+    covered(maps:get(N, Assignments), P)
   end, Ms);
 matches_record(C, _Assignments) -> matches_wildcard(C).
 
 matches_tuple(#alpaca_tuple{values=Patterns}, _TSize, ElemIx, TConstr) ->
   Pattern = lists:nth(ElemIx, Patterns),
-  covered(TConstr, [Pattern]); 
+  covered(TConstr, Pattern);
 matches_tuple(Other, _, _, _) -> matches_wildcard(Other).
 
 matches_unit({unit, _}) -> true;
@@ -192,63 +201,3 @@ matches_unit(C)         -> matches_wildcard(C).
 matches_wildcard({'_', _})       -> true;
 matches_wildcard({symbol, _, _}) -> true;
 matches_wildcard(_)              -> false.
-
-%% 1. Find all match expressions and pattern matching function heads.
-%% For each match expression/function head:
-%% 1.1 Collect all clauses.
-%% 1.2 If any of the clauses is a wildcard, we are done. Otherwise go to 1.3.
-%% 1.3 If any of the clauses contains a guard, print a warning. Otherwise go to 1.4
-%% 1.4 Use the clauses to reconstruct which type it must be.
-%% 1.5. Generate the "principal" patterns for the type, i.e.
-%%      the patterns that must be covered for the match to be total.
-%% 1.6 Check that all the principal patterns are covered. 
-%%
-%% Note 1.2: We do not deal with guards, since they can be arbitrary expressions.
-%% Here are two examples that both cover the full set of integers:
-%%   match i with
-%%     x, x < 50 -> :ok
-%%     x, x = 50 -> :ok
-%%     x, x > 50 -> :ok
-%%
-%%   match i with
-%%     x, x % 2 = 0 -> :ok
-%%     x, x % 2 = 1 -> :ok
-%%
-%% Note 1.4: We don't handle union types!
-%%
-%% Principal patterns, enums:
-%% type color = Red | Green | Blue
-%% -- principal patterns = Red + Green + Blue
-%%
-%% Principal patterns, complex functions:
-%% type gimp = Surface color color
-%%
-%% match g with
-%%   Surface Red Red -> :ok
-%%   Surface Red Green -> :ok
-%%   Surface Red Blue -> :ok
-%%   Surface Green Red -> :ok
-%%   Surface Green Green -> :ok
-%%   Surface Green Blue -> :ok
-%%   Surface Blue Red -> :ok
-%%   Surface Blue Green -> :ok
-%%   Surface Blue Blue -> :ok
-%%
-%% Principal patterns, polymorphic data structures:
-%% type List 'a = Nil | Cons 'a 
-%% -- principal patterns = enum patterns + \/ x:'a Cons x
-%% Note: If we don't have any information about 'a, totality is;  Nil + Cons _
-%%
-%% Princial patterns, recursive data types:
-%% type Tree = Leaf color | Node Tree Tree
-%%
-%% match t with
-%%   Leaf _ -> :ok
-%% | Tree Leaf Leaf ->kk
-%% | Tree _ _ > :ok
-%%
-%% What about first checking all top level constructors, assuming that all
-%% arguments are wildcards. If that passes the totality checker, then go through
-%% one constructor at a time, put all arguments into a tuple, and check if that
-%% totality checks. Or should we totality check the arguments one at a time?
-%%
